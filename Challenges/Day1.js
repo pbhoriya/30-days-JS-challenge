2cbace1c47266737501875f946ab572570e1ac14
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-15 05:20:54 +0000

    Data Types

---
+++ Challenges/Day1.js
@@ -0,0 +1,40 @@
+//Activity 1 : Variables
+var num1 = 100
+console.log(num1)
+
+let str1="User"
+console.log(str1)
+
+//Activity 2 : Const
+const bool=true
+console.log(bool)
+
+//Activity 3 : Data Types
+let num2 = 90
+let str2 = "group"
+let str3 = new String('group')
+let obj = new Object()
+let bool1=true
+let arr1=[2,4,6,8]
+
+console.log(typeof(num2))
+console.log(typeof(str2))
+console.log(typeof(str3))
+console.log(typeof(obj))
+console.log(typeof(bool1))
+console.log(typeof(arr1))
+
+// Activity 4 : Reassigning Variables
+let var1= 100
+console.log(var1)
+var1=200
+console.log(var1)
+
+// Activity 5 : Understanding const
+const con1=100
+console.log(con1)
+con1=200
+console.log(con1)//Error is :- Assignment to  constant variable
+
+
+
