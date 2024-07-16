ee4185194cbf9fd0558c292b53d4c1b580e71ff8
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-16 09:15:03 +0000

    Loops

---
+++ Challenges/Day4.js
@@ -0,0 +1,84 @@
+// //Activity 1
+// let num1=1;
+// for(num1;num1<=10;num1++)
+// {
+//     console.log(num1);
+// }
+
+// let num1=1;
+// for(num1;num1<=10;num1++)
+// {
+//     let num2=5*num1;
+//     console.log("5 *",num1 ,"=" ,num2)
+// }
+
+//Activity 2
+// let num1=1;
+// let num2=0;
+// while(num1<=10)
+// {
+//      num2=num2+num1;
+//      num1++;
+// }
+// console.log(num2);
+
+// let num1=10;
+// while(num1>0)
+// {
+//     console.log(num1);
+//     num1--;
+// }
+
+//Activity 4
+
+// let num=1
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while(num<6);
+
+// let num=3;
+// let factorial=1;
+// do{
+//     factorial=factorial*num;
+//     num--
+// }
+// while(num>0);
+// console.log(factorial);
+
+// let row,col;
+// for(row=0;row<5;row++)
+// {
+//     let rows='';
+//     for(col=0;col<=row;col++)
+//     {
+//         rows+='* ';
+//        }
+//        console.log(rows);
+
+// }
+
+//Activity 5
+// let num=1;
+// for(num;num<=10;num++)
+// {
+//     if(num==5)
+//     {
+//         continue;
+//     }
+//     console.log(num);
+// }
+
+let num=1;
+for(num;num<=10;num++)
+{
+    if(num==7)
+    {
+        console.log(num);
+        break;
+    }
+    console.log(num);
+
+}
+
