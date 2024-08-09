991ed3b2c7f0c7aee23a7609db2d4d408bc7b00d
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-08-09 11:26:04 +0000

    Array

---
+++ Challenges/Day6.js
@@ -0,0 +1,15 @@
+//Activity 1
+const myArr=[1,2,3,4,5]
+console.log(myArr)
+console.log(myArr[0])
+console.log(myArr[4])
+
+//Activity 2
+myArr.push(6)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+myArr.unshift(0);
+console.log(myArr)
+myArr.shift();
+console.log(myArr)
