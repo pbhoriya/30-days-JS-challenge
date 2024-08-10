00805b78fba611f39a882e820dc1e2e617b68b6a
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-08-10 11:34:13 +0000

    arrays

--- Challenges/Day6.js
+++ Challenges/Day6.js
@@ -1,4 +1,4 @@
-//Activity 1
+// //Activity 1
 const myArr=[1,2,3,4,5]
 console.log(myArr)
 console.log(myArr[0])
@@ -13,3 +13,51 @@ myArr.unshift(0);
 console.log(myArr)
 myArr.shift();
 console.log(myArr)
+
+
+// //Activity3
+const myArr1=[1,2,3,4,5,6]
+const doubleArr=myArr1.map(function(number){
+    return number*2;
+});
+console.log(doubleArr)
+
+const evenno=myArr1.filter(function(number){
+    return number%2==0
+})
+console.log(evenno);
+
+const sum=myArr1.reduce(function(accumulator, currentValue){
+    return accumulator+currentValue
+})
+console.log("The sum of array is",sum);
+
+// //Activity 4
+const myArr2=[1,2,3,4,5]
+for(let i=0;i<myArr2.length;i++)
+{
+    console.log(myArr2[i]);
+}
+console.log();
+console.log(myArr2.length)
+console.log();
+myArr2.forEach(function(number){
+    console.log(number);
+})
+
+// Activity 5
+const matrixArr=[[1,2],[3,4,7],[5,6]];
+console.log(matrixArr);
+for(let i=0;i<matrixArr.length;i++)
+{
+    console.log(matrixArr[i]);
+}
+console.log(matrixArr[2][0]);//pehle vala box index k  liye dusra vala block number k liye
+// below method is use to print array elements sinle single
+// for(let i=0;i<matrixArr.length;i++)
+//     {
+//         for(let j=0;j<matrixArr.length-1;j++)
+//         {
+//         console.log(matrixArr[i][j]);
+//         }
+//     }
